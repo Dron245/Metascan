28dadb11f0187c299ee18379193d64f49a87e3fb
Dron245 <Dron245@yandex.ru>
2023-02-09 15:55:32 +0300

    0902-1555

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,20 @@
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
+
+//document.addEventListener("click", documentActions);
+
+// const item =document.querySelectorAll('.menu__item');
+// item.forEach(element => {
+// 	element.addEventListener("click", qwe)
+// 	function qwe() {
+// 		element.previousElementSibling.classList.toggle('sss')
+// 	}
+// });
+// function documentActions(e) {
+// 	const targetElement = e.target;
+// 	if(targetElement.closest('.menu__item')){
+// 		console.log(targetElement);
+// 		targetElement.previousElementSibling.classlist.toggle('preview')
+// 	}
+// }
